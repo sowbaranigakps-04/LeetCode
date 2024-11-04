bb9ea4c2e55b4ee4f852c14db58713b46ee41aad
sowbaranigakps-04 <167520847+sowbaranigakps-04@users.noreply.github.com>
2024-11-04 16:04:22 +0530

    Time: 2 ms (71.52%), Space: 42.2 MB (63%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,71 +1,51 @@
-class Solution {
-    public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int vis[][] = new int[m][n];
-        Queue<int[]> q = new LinkedList<>();
-        int fo = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) fo++;
-                if (grid[i][j] == 2) {
-                    q.add(new int[]{i, j});
-                }
-            }
-        }
-
-        if (fo == 0) return 0;
-        
-        int coun = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            int flag = 0;
-            
-            for (int i = 0; i < size; i++) {
-                int[] curr = q.poll();
-                int r = curr[0];
-                int c = curr[1];
-                vis[r][c] = 1;
-                
-                if (r + 1 < m && grid[r + 1][c] == 1 && vis[r + 1][c] != 1) {
-                    q.add(new int[]{r + 1, c});
-                    grid[r + 1][c] = 2;
-                    fo--;
-                    flag = 1;
-                }
-                
-                if (r - 1 >= 0 && grid[r - 1][c] == 1 && vis[r - 1][c] != 1) {
-                    q.add(new int[]{r - 1, c});
-                    grid[r - 1][c] = 2;
-                    fo--;
-                    flag = 1;
-                }
-                
-                if (c + 1 < n && grid[r][c + 1] == 1 && vis[r][c + 1] != 1) {
-                    q.add(new int[]{r, c + 1});
-                    grid[r][c + 1] = 2;
-                    fo--;
-                    flag = 1;
-                }
-                
-                if (c - 1 >= 0 && grid[r][c - 1] == 1 && vis[r][c - 1] != 1) {
-                    q.add(new int[]{r, c - 1});
-                    grid[r][c - 1] = 2;
-                    fo--;
-                    flag = 1;
-                }
-            }
-            
-            if (flag == 1) {
-                coun++;
-            }
-        }
-        
-        if (fo > 0) {
-            return -1;
-        }
-        
-        return coun;
+class Pair{
+    int row;
+    int col;
+    int level;
+    Pair(int _row,int _col,int _level){
+        this.row=_row;
+        this.col = _col;
+        this.level = _level;
+    }//Pair(2,3,9)
+}
+class Solution{
+    public int orangesRotting(int [][] grid){
+       Queue<Pair> q = new LinkedList<>();
+       int m = grid.length;
+       int n = grid[0].length;
+       int fr =0;
+       int [][] vis = new int[m][n];
+       for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+               if(grid[i][j]==2){
+                   q.offer(new Pair(i,j,0));
+                   vis[i][j]=1;
+               }
+               if(grid[i][j]==1) fr++;
+           }
+       }
+       int max_level = 0;
+       int [] drow ={0,1,0,-1};
+       int [] dcol ={1,0,-1,0};
+       while(!q.isEmpty()){
+           int r = q.peek().row;
+           int c = q.peek().col;
+           int curr_level = q.peek().level;
+           q.poll();
+           max_level = Math.max(curr_level,max_level);
+           for(int i=0;i<4;i++){
+               int nrow= r+drow[i];
+               int ncol = c + dcol[i];
+               if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                   q.offer(new Pair(nrow,ncol,curr_level+1));
+                   vis[nrow][ncol]=1;
+                   fr--;
+               }
+               
+           }
+           
+       }
+       if(fr!=0) return -1;
+       return max_level;
     }
 }
