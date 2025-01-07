20ccfc4278471e8516242d37410d2acdae6eddcd
sowbaranigakps-04 <sowbaranigakps04@gmail.com>
2025-01-07 14:12:03 +0530

    Time: 1 ms (46.07%), Space: 40.7 MB (79.87%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,18 +1,30 @@
 class Solution {
     public boolean isUgly(int n) {
-        if(n == 0)
+        if(n==0)
             return false;
-        while(n!=1)
-        {
-            if(n%2==0)
+        
+        else{
+
+            while(n%2==0)
+            {
                 n=n/2;
-            else if(n%3==0)
+            }
+
+             while(n%3==0)
+            {
                 n=n/3;
-            else if(n%5==0)
+            }
+
+             while(n%5==0)
+            {
                 n=n/5;
+            }
+
+            if(n==1)
+                return true;
             else
                 return false;
         }
-        return true;
+        
     }
 }
