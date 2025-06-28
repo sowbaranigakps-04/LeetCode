04d2f86ea5ce147ea3b289952545136dd1119c44
sowbaranigakps-04 <sowbaranigakps04@gmail.com>
2025-06-28 22:12:21 +0530

    Time: 2 ms (80.62%), Space: 46.5 MB (5.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int index = 0;
+        int j = -1;
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]!=0)
-            {
-                nums[index++] = nums[i];
-
+            if(nums[i] == 0){
+                j = i;
+                break;
             }
         }
-        while(index<nums.length)
+        if(j != -1)
         {
-            nums[index++] = 0;
-          
+        for(int i=j+1;i<nums.length;i++)
+        {
+            if(nums[i] != 0)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j = j+1;
+            }
         }
-        
+        }
     }
 }
