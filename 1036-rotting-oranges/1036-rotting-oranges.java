ca3f1d0a6bad27a58143028d61f6db0e819f1cc0
sowbaranigakps-04 <sowbaranigakps04@gmail.com>
2025-08-02 14:27:37 +0530

    Time: 2 ms (67.31%), Space: 42.5 MB (61.95%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,51 +1,61 @@
 class Pair{
     int row;
     int col;
-    int level;
-    Pair(int _row,int _col,int _level){
-        this.row=_row;
-        this.col = _col;
-        this.level = _level;
-    }//Pair(2,3,9)
+    int min;
+    Pair(int row,int col,int min)
+    {
+        this.col=col;
+        this.row=row;
+        this.min=min;
+
+    }
 }
-class Solution{
-    public int orangesRotting(int [][] grid){
-       Queue<Pair> q = new LinkedList<>();
-       int m = grid.length;
-       int n = grid[0].length;
-       int fr =0;
-       int [][] vis = new int[m][n];
-       for(int i=0;i<m;i++){
-           for(int j=0;j<n;j++){
-               if(grid[i][j]==2){
-                   q.offer(new Pair(i,j,0));
-                   vis[i][j]=1;
-               }
-               if(grid[i][j]==1) fr++;
-           }
-       }
-       int max_level = 0;
-       int [] drow ={0,1,0,-1};
-       int [] dcol ={1,0,-1,0};
-       while(!q.isEmpty()){
-           int r = q.peek().row;
-           int c = q.peek().col;
-           int curr_level = q.peek().level;
-           q.poll();
-           max_level = Math.max(curr_level,max_level);
-           for(int i=0;i<4;i++){
-               int nrow= r+drow[i];
-               int ncol = c + dcol[i];
-               if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                   q.offer(new Pair(nrow,ncol,curr_level+1));
-                   vis[nrow][ncol]=1;
-                   fr--;
-               }
-               
-           }
-           
-       }
-       if(fr!=0) return -1;
-       return max_level;
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int fruit=0,max=0;
+        int vis[][] = new int[m][n];
+        Queue<Pair> q1 = new LinkedList<>();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q1.add(new Pair(i,j,0));
+                    vis[i][j]=1;
+                }
+                if(grid[i][j]==1)
+                {
+                    fruit++;
+                }
+            }
+        }
+        int drow[]={-1,1,0,0};
+        int dcol[]={0,0,1,-1};
+        while(!q1.isEmpty())
+        {
+            int col=q1.peek().col;
+            int row=q1.peek().row;
+            int min=q1.peek().min;
+            max=min;
+            q1.poll();
+            for(int i=0;i<4;i++)
+            {
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]!=1 && grid[nrow][ncol]==1)
+                {
+                    q1.add(new Pair(nrow,ncol,min+1));
+                    fruit--;
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+        if(fruit==0)
+          return max;
+        return -1; 
+        
     }
 }
