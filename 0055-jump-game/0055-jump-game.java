bca7503afe7577f037982e7c6244d59332b61d8d
Sowbaraniga K <sowbaranigakps04@gmail.com>
2026-09-03 10:44:15 +0530

    Time: 2 ms (89.02%), Space: 47.2 MB (99.18%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int finalPosition = nums.length - 1;
+
+        for(int idx = nums.length-2; idx>=0; idx--){
+            if(idx+nums[idx] >= finalPosition)
+                finalPosition = idx;
+        }
+
+        return finalPosition == 0;
+    }
+}
